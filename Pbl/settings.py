acf2eb4d9bbe667fb0408dd7dc94240634ee4412
Sarang Kulkarni <sarangkulkarni567@gmail.com>
2022-10-24 20:49:52 +0530

    Update settings.py

--- Pbl/settings.py
+++ Pbl/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bm7$4be=%)ihi609-*w5*d&o7#6r0h721l$^_qrn3#jpskg4)$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
